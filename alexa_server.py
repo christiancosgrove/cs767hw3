adf8df4c1af55e3fd2d090b0dbb1df760dfaef28
Darius Irani <dirani2@jhu.edu>
2020-02-20 17:29:15 -0500

    command invokation for alexa server

--- alexa_server.py
+++ alexa_server.py
@@ -1,23 +1,20 @@
 from flask import Flask
 from flask_ask import Ask, statement, question, session
 import logging
-from parlai.scripts.interactive import setup_args, interactive
+import os
 import random
+import subprocess
 
 app = Flask(__name__)
 ask = Ask(app, "/")
 
-@ask.intent("AskDeepPavlov", mapping={'input':'raw_input'})
+@ask.intent("AskDeepPavlov", mapping={'user_input':'raw_input'})
 def response_from_model():
-	
-    # random.seed(42)
-    # parser = setup_args()
-    # opt = parser.parse_args()
-    # interactive(opt, print_parser=parser)
-
-    
-	return statement(response)
-
+    response = subprocess.check_output('user_input').decode()
+    return question(response)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
+    cmd = 'python -m parlai.scripts.interactive -mf zoo:convai2/seq2seq/convai2_self_seq2seq_model -m legacy:seq2seq:0'
+    # cmd = 'python3 examples/interactive.py -m hred/hred -mf test_hred.checkpoint.checkpoint'
+    os.system(cmd)
