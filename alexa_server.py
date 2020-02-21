f8af1a3ced7c1c465bce99fa84cea46d0c9d190b
jlee581 <37848491+jlee581@users.noreply.github.com>
2020-02-21 01:13:30 -0500

    Update alexa_server.py

--- alexa_server.py
+++ alexa_server.py
@@ -8,21 +8,22 @@ import subprocess
 app = Flask(__name__)
 ask = Ask(app, "/")
 
-# @ask.launch
-# def welcome():
-# 	#cmd = 'python3 examples/interactive.py -m hred/hred -mf test_hred.checkpoint.checkpoint'
-# 	#os.system(cmd)
-# 	#welcome_message = 'Hello there, would you like the news?'
-# 	#return question(welcome_message)
-# 	model_setup = "model finished setting up"
-# 	return statement(model_setup)
+@ask.launch
+def welcome():
+	#cmd = 'python3 examples/interactive.py -m hred/hred -mf test_hred.checkpoint.checkpoint'
+	#os.system(cmd)
+	#welcome_message = 'Hello there, would you like the news?'
+	#return question(welcome_message)
+	model_setup = "connected"
+	return statement(model_setup)
 
 @ask.intent("AskDeepPavlov", mapping={'user_input':'raw_input'})
 def response_from_model():
 	cmd = 'python3 examples/interactive.py -m hred/hred -mf test_hred.checkpoint.checkpoint'
 	os.system(cmd)
+	user_input = 'Hello'
 	while True:
-		response = subprocess.check_output('user_input').decode()
+		response = subprocess.check_output(user_input).decode()
 		response = response[7:] # Should get rid of [HRED]: 
 		return question(response).reprompt("This is a debugger to check we're still in HRED")
 
