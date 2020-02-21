76c30ee6b9b7f2b160fb26556e4e4c096e42c3b2
jlee581 <37848491+jlee581@users.noreply.github.com>
2020-02-21 00:06:37 -0500

    Update alexa_server.py

--- alexa_server.py
+++ alexa_server.py
@@ -21,7 +21,8 @@ def welcome():
 def response_from_model():
 	while True:
 		response = subprocess.check_output('user_input').decode()
-		return question(response_from_model)
+		response = response[7:] # Should get rid of [HRED]: 
+		return question(response).reprompt("This is a debugger to check we're still in HRED")
 
 if __name__ == '__main__':
 	app.run(debug = True)
