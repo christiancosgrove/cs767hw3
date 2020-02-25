0d13f5aa18c46333c5ac1dc0ee3a96de06a6ab6b
Christian Cosgrove <christian.cosgrove@gmail.com>
2020-02-25 01:44:26 -0500

    talking with alexa works

--- alexa_server2.py
+++ alexa_server2.py
@@ -9,30 +9,42 @@ import subprocess
 from subprocess import PIPE
 from time import sleep
 
-global p
-
 app = Flask(__name__)
 ask = Ask(app, "/")
+
+
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
+
 @ask.launch
 def welcome():
 	model_setup = "connected"
-	return statement(model_setup)
+	return question(model_setup)
 
 @ask.intent("AskDeepPavlov", mapping={'user_input':'raw_input'})
 def response_from_model(user_input):
-	while True:
+	print("USER INPUT IS ", user_input)
+	
+	target = 'Enter Your Message'
+	m = ''
+	while m[:len(target)] != target and p.poll() is None:
 		p.stdin.write(b'a line\n')
 		p.stdin.flush()
-		response = p.stdout.readline()
-		response = subprocess.check_output(user_input).decode()
-		response = response[7:] # Should get rid of [HRED]: 
-		return question(response).reprompt("This is a debugger to check we're still in HRED")
+		m = p.stdout.readline().decode()
+	
+	p.stdin.write(user_input.encode())
+	p.stdin.flush()
+	response = p.stdout.readline().decode()
+	print('RESPONSE', response)
+	response = response[7:] # Should get rid of [HRED]: 
+	return question(response).reprompt("This is a debugger to check we're still in HRED")
 
 
 def start_model():
+	global p
 	p = subprocess.Popen(["python","examples/interactive.py","-m" ,"hred", "-mf","test_hred.checkpoint"], stdin=PIPE, stdout=PIPE, bufsize=1)
 	sleep(10)
 	p.stdout.flush()
+	print('flushed!')#, p.stdout.read())
 
 if __name__ == '__main__':
 	start_model()
